82c9c5532bf242e95377f34d473c4eb37221b728
sednabcn <ruperto.bonet@modelphysmat.com>
2025-10-17 19:43:52 +0100

    Fix imports

--- tests/mocks/__init__.py
+++ tests/mocks/__init__.py
@@ -2,6 +2,6 @@
 Mock data and objects for testing
 """
 
-from .mock_data import mock_data
+from .mock_data import get_mock_cv, get_mock_job, get_mock_sprint, create_custom_mock_analysis
 
 __all__ = ["mock_data"]
